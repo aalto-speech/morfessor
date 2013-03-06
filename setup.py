266d268f4463f2ffeb6c0d79d9dd29b409c5510e
Peter Smit <peter.smit@aalto.fi>
2013-03-06 13:34:26 +0200

    Remove progressbar as a requirement

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ main_py = open('morfessor.py').read()
 metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", main_py))
 
 requires = [
-    'progressbar',
+#    'progressbar',
 ]
 
 setup(name='Morfessor',
