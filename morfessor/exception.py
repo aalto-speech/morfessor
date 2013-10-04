338d654024dcb5359261d14cb0751bb589c41c1f
Sami Virpioja <sami.virpioja@lingsoft.fi>
2013-10-04 17:46:30 +0300

    fixed python3 support

--- morfessor/exception.py
+++ morfessor/exception.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 class MorfessorException(Exception):
     """Base class for exceptions in this module."""
     pass
@@ -10,23 +12,20 @@ class ArgumentException(Exception):
 class InvalidCategoryError(MorfessorException):
     def __init__(self, category):
         super(InvalidCategoryError, self).__init__(
-            self,
-            u'This model does not recognize the category {}'.format(
+            self, 'This model does not recognize the category {}'.format(
                 category))
 
 
 class InvalidOperationError(MorfessorException):
     def __init__(self, operation, function_name):
         super(InvalidOperationError, self).__init__(
-            self,
-            (u'This model does not have a method ' +
-             u'{}, and therefore cannot perform operation "{}"'.format(
-                function_name, operation)))
+            self, ('This model does not have a method {}, ' +
+                   'and therefore cannot perform operation "{}"').format(
+                function_name, operation))
 
 
 class UnsupportedConfigurationError(MorfessorException):
     def __init__(self, reason):
         super(UnsupportedConfigurationError, self).__init__(
-            self,
-            u'This operation is not supported in this program configuration. '
-            u'Reason: {}.'.format(reason))
+            self, ('This operation is not supported in this program ' +
+                   'configuration. Reason: {}.').format(reason))
